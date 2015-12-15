2a072a4539c6c1f25462d666af93262ee937369b
Bastien Falcou <bastien@fueled.com>
2015-12-15 14:43:15 +0100

    feat(Project): updated project architecture.

---
+++ Memz/Code/Resources/Supporting Files/constants.h
@@ -0,0 +1,13 @@
+//
+//  constants.h
+//  Memz
+//
+//  Created by Bastien Falcou on 12/15/15.
+//  Copyright © 2015 Falcou. All rights reserved.
+//
+
+#ifndef constants_h
+#define constants_h
+
+
+#endif /* constants_h */
