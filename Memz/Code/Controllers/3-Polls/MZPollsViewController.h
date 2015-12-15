997a9942724b34c1fc1ea878dc8a89e1454cbdac
Bastien Falcou <bastien@fueled.com>
2015-12-16 00:56:21 +0100

    feat(MVPollsViewController): implemented and simple version of shrinkable view.

---
+++ Memz/Code/Controllers/3-Polls/MZPollsViewController.h
@@ -0,0 +1,13 @@
+//
+//  MZPollsViewController.h
+//  Memz
+//
+//  Created by Bastien Falcou on 12/16/15.
+//  Copyright © 2015 Falcou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MZPollsViewController : UIViewController
+
+@end
