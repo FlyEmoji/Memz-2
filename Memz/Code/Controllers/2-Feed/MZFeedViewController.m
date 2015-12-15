a798b5c513c0c67532aa3d16f07668480f7216a0
Bastien Falcou <bastien@fueled.com>
2015-12-15 20:13:47 +0100

    refactor(Project): repo and app groups architecture.

---
+++ Memz/Code/Controllers/2-Feed/MZFeedViewController.m
@@ -0,0 +1,13 @@
+//
+//  MZFeedViewController.m
+//  Memz
+//
+//  Created by Bastien Falcou on 12/15/15.
+//  Copyright © 2015 Falcou. All rights reserved.
+//
+
+#import "MZFeedViewController.h"
+
+@implementation MZFeedViewController
+
+@end
