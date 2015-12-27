b96432539cd5dba054648d726fc8c9fe116b5656
Bastien Falcou <bastien@fueled.com>
2015-12-28 00:22:50 +0100

    feat(MZQuizManager): implemented simple quiz manager.

---
+++ Memz/Code/Helpers/Classes/MZQuizManager/MZQuizManager.h
@@ -0,0 +1,24 @@
+//
+//  MZQuizManager.h
+//  Memz
+//
+//  Created by Bastien Falcou on 12/28/15.
+//  Copyright © 2015 Falcou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MZQuizz.h"
+
+@interface MZQuizManager : NSObject
+
+@property (nonatomic, assign) NSTimeInterval quizzDelay;
+@property (nonatomic, assign, readonly) BOOL isActive;
+
++ (MZQuizManager *)sharedManager;
+
+- (void)startManager;
+- (void)stopManager;
+
+- (MZQuizz *)generateQuiz;
+
+@end
---
+++ Memz/Code/Helpers/Classes/MZQuizManager/MZQuizManager.m
@@ -0,0 +1,38 @@
+//
+//  MZQuizManager.m
+//  Memz
+//
+//  Created by Bastien Falcou on 12/28/15.
+//  Copyright © 2015 Falcou. All rights reserved.
+//
+
+#import "MZQuizManager.h"
+
+@implementation MZQuizManager
+
++ (MZQuizManager *)sharedManager {
+	static MZQuizManager *_sharedManager = nil;
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		_sharedManager = [[self alloc] init];
+	});
+	return _sharedManager;
+}
+
+#pragma mark - Public Methods
+
+- (void)startManager {
+	_isActive = YES;
+}
+
+- (void)stopManager {
+	_isActive = NO;
+}
+
+- (MZQuizz *)generateQuiz {
+	return nil;
+}
+
+#pragma mark - Private Methods
+
+@end
