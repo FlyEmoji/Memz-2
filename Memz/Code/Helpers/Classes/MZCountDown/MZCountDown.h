c6ebf7d08273840c1cad5917ec719a6c66c6f024
Bastien Falcou <bastien@fueled.com>
2015-12-29 20:48:27 +0100

    feat(MZCountDown): implemented count down.

---
+++ Memz/Code/Helpers/Classes/MZCountDown/MZCountDown.h
@@ -0,0 +1,34 @@
+//
+//  MZCountDown.h
+//  Memz
+//
+//  Created by Bastien Falcou on 12/29/15.
+//  Copyright © 2015 Falcou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol MZCountDownDelegate;
+
+@interface MZCountDown : NSObject
+
+@property (nonatomic, weak) id<MZCountDownDelegate> delegate;
+@property (nonatomic, assign, readonly) BOOL isRunning;
+@property (nonatomic, assign, readonly) NSTimeInterval remainingTime;
+
++ (instancetype)countDownWithDuration:(NSTimeInterval)duration delegate:(id<MZCountDownDelegate>)delegate;
+- (instancetype)initWithDuration:(NSTimeInterval)duration delegate:(id<MZCountDownDelegate>)delegate;
+
+- (void)fire;
+- (void)stop;
+
+@end
+
+@protocol MZCountDownDelegate <NSObject>
+
+@optional
+
+- (void)countDownDidChange:(MZCountDown *)countDown remainingTime:(NSTimeInterval)remainingTime totalTime:(NSTimeInterval)totalTime;
+- (void)countDownDidEnd:(MZCountDown *)countDown;
+
+@end
