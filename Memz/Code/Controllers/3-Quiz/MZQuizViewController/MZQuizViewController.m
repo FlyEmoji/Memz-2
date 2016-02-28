3e0adeba32ecdfde63f5a7dbddf08d2f7088e191
Bastien Falcou <bastien@fueled.com>
2016-02-28 11:41:08 -0500

    fix(MZQuizViewController): quiz table view header should not be in edit mode.

--- Memz/Code/Controllers/3-Quiz/MZQuizViewController/MZQuizViewController.m
+++ Memz/Code/Controllers/3-Quiz/MZQuizViewController/MZQuizViewController.m
@@ -143,6 +143,7 @@ MZCountDownDelegate>
 	}
 
 	self.tableViewHeader.word = self.response.word;
+	self.tableViewHeader.headerType = MZWordDescriptionHeaderTypeReadonly;
 	self.tableViewHeader.frame = CGRectMake(0.0f, 0.0f, self.tableView.frame.size.width, self.tableView.frame.size.height / 4.0f);
 
 	[self.tableView reloadData];
