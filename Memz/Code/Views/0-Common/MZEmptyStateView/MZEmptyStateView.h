e94814148446e04baa9719d8230e7ea4fc3e3b05
Bastien Falcou <bastien@fueled.com>
2016-05-18 18:26:11 -0400

    feat(MZEmptyStateView): implemented generic reusable empty state view.

---
+++ Memz/Code/Views/0-Common/MZEmptyStateView/MZEmptyStateView.h
@@ -0,0 +1,18 @@
+//
+//  MZEmptyStateView.h
+//  Memz
+//
+//  Created by Bastien Falcou on 5/18/16.
+//  Copyright © 2016 Falcou. All rights reserved.
+//
+
+#import "MZNibView.h"
+
+IB_DESIGNABLE
+
+@interface MZEmptyStateView : MZNibView
+
+@property (nonatomic, strong) IBInspectable UIImage *emptyStateImage;
+@property (nonatomic, strong) IBInspectable NSString *emptyStateDescription;
+
+@end
---
+++ Memz/Code/Views/0-Common/MZEmptyStateView/MZEmptyStateView.m
@@ -0,0 +1,45 @@
+//
+//  MZEmptyStateView.m
+//  Memz
+//
+//  Created by Bastien Falcou on 5/18/16.
+//  Copyright © 2016 Falcou. All rights reserved.
+//
+
+#import "MZEmptyStateView.h"
+
+@interface MZEmptyStateView ()
+
+@property (nonatomic, weak) IBOutlet UIImageView *emptyStateImageView;
+@property (nonatomic, weak) IBOutlet UILabel *emptyStateLabel;
+
+@end
+
+@implementation MZEmptyStateView
+
+- (void)awakeFromNib {
+	[super awakeFromNib];
+
+	[self updateElements];
+}
+
+- (void)updateElements {
+	self.emptyStateImageView.image = self.emptyStateImage;
+	self.emptyStateLabel.text = self.emptyStateDescription;
+}
+
+#pragma mark - Custom Setters
+
+- (void)setEmptyStateImage:(UIImage *)emptyStateImage {
+	_emptyStateImage = emptyStateImage;
+
+	self.emptyStateImageView.image = emptyStateImage;
+}
+
+- (void)setEmptyStateDescription:(NSString *)emptyStateDescription {
+	_emptyStateDescription = emptyStateDescription;
+
+	self.emptyStateLabel.text = emptyStateDescription;
+}
+
+@end
